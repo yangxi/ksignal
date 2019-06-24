fdc01074c776c3e8acbfaa4c8037570c908acfb2
Xi Yang <hiyangxi@gmail.com>
2019-06-24 23:23:16 +1000

    Export the shim_curr_task to the flag_signal parameter.

--- ksignal.c
+++ ksignal.c
@@ -22,13 +22,16 @@
 #define NR_SHIM_CPU (100)
 
 DECLARE_PER_CPU(unsigned int, shim_curr_flag);
+DECLARE_PER_CPU(unsigned long, shim_curr_task);
 DECLARE_PER_CPU(unsigned long *, shim_signal);
+
+static unsigned long task_signal[NR_SHIM_CPU];
+module_param_array(task_signal, ulong, NULL, 0644);
+MODULE_PARM_DESC(task_signal, "Per-CPU signal shows the running task.");
 
 static unsigned long signal_page[NR_SHIM_CPU];
 module_param_array(signal_page, ulong, NULL, 0644);
 MODULE_PARM_DESC(signal_page, "Per-CPU signal shows the running task.");
-
-
 
 static unsigned long flag_signal[NR_SHIM_CPU];
 module_param_array(flag_signal, ulong, NULL, 0644);
@@ -37,16 +40,23 @@ MODULE_PARM_DESC(flag_signal, "Per-CPU signal shows the status of the current ru
 static int ksignals_init(void)
 {
   int cpu;
+  volatile unsigned long * shim_task_signal = NULL;
   volatile unsigned int * shim_flag_signal = NULL;
   volatile unsigned long * signal_page_address = NULL;
 
   for_each_possible_cpu (cpu) {
     if (cpu >= NR_SHIM_CPU)
       break;
+
+    shim_task_signal = per_cpu_ptr(&shim_curr_task, cpu);
     shim_flag_signal = per_cpu_ptr(&shim_curr_flag, cpu);
     signal_page_address = per_cpu(shim_signal, cpu);
+
+    task_signal[cpu] = (unsigned long)__pa(shim_task_signal);
     flag_signal[cpu] = (unsigned long)__pa(shim_flag_signal);
     signal_page[cpu] = (unsigned long)__pa(signal_page_address);
+
+    pr_debug("CPU %d SHIM_TASK, va %p, pa %lx\n", cpu, shim_task_signal, __pa(shim_task_signal));
     pr_debug("CPU %d SHIM_SIGNAL, va %lx, pa %lx\n", cpu, (unsigned long)signal_page_address, __pa(signal_page_address));
   }
   return 0;
